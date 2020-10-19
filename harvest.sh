de6f8161c1de5be9d8f500d2ae53446de400c4a3
Shane Vitarana <shanev@gmail.com>
2020-10-19 18:34:36 -0400

    updated genesis ceremony (harvest)

--- harvest.sh
+++ harvest.sh
@@ -7,7 +7,7 @@ rm -rf $CONFIG/gentx && mkdir $CONFIG/gentx
 
 for i in $NETWORK/gentx/*.json; do
   echo $i
-  staked add-genesis-account $(jq -r '.value.msg[0].value.delegator_address' $i) $(jq -r '.value.msg[0].value.value.amount' $i)$(jq -r '.value.msg[0].value.value.denom' $i)
+  staked add-genesis-account $(jq -r '.body.messages[0].delegator_address' $i) $(jq -r '.body.messages[0].value.amount' $i)$(jq -r '.body.messages[0].value.denom' $i)
   cp $i $CONFIG/gentx/
 done
 staked collect-gentxs
