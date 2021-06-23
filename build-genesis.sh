02bb475a761df520dfa33a73950bf06f8470fb40
Shane Vitarana <shanev@gmail.com>
2021-06-23 11:19:26 -0400

    Changed unbonding period to 7 days

--- build-genesis.sh
+++ build-genesis.sh
@@ -3,13 +3,14 @@
 CONFIG=~/.starsd/config
 NETWORK=cygnusx-1
 
-echo "Writing genesis accounts..."
-sh ./accounts.sh
+# echo "Writing genesis accounts..."
+# sh ./accounts.sh
 
-echo "Writing validator transactions..."
-sh ./validators.sh
+# echo "Writing validator transactions..."
+# sh ./validators.sh
+
+cp $CONFIG/genesis.json $NETWORK
 
 echo "Process default genesis changes.."
 sed -i '' 's/"default_send_enabled": true/"default_send_enabled": false/g' $NETWORK/genesis.json
-
-cp $CONFIG/genesis.json $NETWORK
+sed -i '' 's/"unbonding_time": "1814400s",/"unbonding_time": "604800s",/g' $NETWORK/genesis.json
