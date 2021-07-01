0622a05310c0e6c312754237c3b29cec5cdb5d4f
Shane Vitarana <shanev@gmail.com>
2021-06-30 23:32:16 -0400

    Update validator coins

--- build-genesis.sh
+++ build-genesis.sh
@@ -3,8 +3,8 @@
 CONFIG=~/.starsd/config
 NETWORK=cygnusx-1
 
-echo "Writing genesis accounts..."
-sh ./accounts.sh
+# echo "Writing genesis accounts..."
+# sh ./accounts.sh
 
 echo "Writing validator transactions..."
 sh ./validators.sh
