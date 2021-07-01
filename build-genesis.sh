72aafb52c9bf5e25e9b15183c2661f0c67f44655
Shane Vitarana <shanev@gmail.com>
2021-06-30 23:16:44 -0400

    Removed genesis changes

--- build-genesis.sh
+++ build-genesis.sh
@@ -10,8 +10,3 @@ echo "Writing validator transactions..."
 sh ./validators.sh
 
 cp $CONFIG/genesis.json $NETWORK
-
-echo "Process default genesis changes.."
-sed -i '' 's/"default_send_enabled": true/"default_send_enabled": false/g' $NETWORK/genesis.json
-sed -i '' 's/"send_enabled": true,/"send_enabled": false,/g' $NETWORK/genesis.json
-sed -i '' 's/"receive_enabled": true/"receive_enabled": false/g' $NETWORK/genesis.json
