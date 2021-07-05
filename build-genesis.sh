f854c2950296e51dcfcc010e19a73f5856749728
Shane Vitarana <shanev@gmail.com>
2021-07-05 18:22:33 -0400

    download hub-4 archive

--- build-genesis.sh
+++ build-genesis.sh
@@ -6,6 +6,10 @@ NETWORK=cygnusx-1
 # echo "Writing genesis accounts..."
 # sh ./accounts.sh
 
+curl -O https://archive.interchain.io/4.0.2/genesis.json
+starsd export-airdrop-snapshot uatom genesis.json snapshot.json
+
+
 echo "Writing validator transactions..."
 sh ./validators.sh
 
