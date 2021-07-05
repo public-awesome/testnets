26702a70047f098b97c2490b721ad7cd462db344
Shane Vitarana <shanev@gmail.com>
2021-07-05 18:30:15 -0400

    updating build genesis script

--- build-genesis.sh
+++ build-genesis.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-CONFIG=~/.starsd/config
-NETWORK=cygnusx-1
+# CONFIG=~/.starsd/config
+# NETWORK=cygnusx-1
 
 # echo "Writing genesis accounts..."
 # sh ./accounts.sh
 
+rm -rf ~/.starsd
+
 curl -O https://archive.interchain.io/4.0.2/genesis.json
 starsd export-airdrop-snapshot uatom genesis.json snapshot.json
 
+starsd import-genesis-accounts-from-snapshot snapshot.json
 
-echo "Writing validator transactions..."
-sh ./validators.sh
+starsd prepare-genesis testnet cygnusx-1
 
-cp $CONFIG/genesis.json $NETWORK
+# echo "Writing validator transactions..."
+# sh ./validators.sh
+
+# cp $CONFIG/genesis.json $NETWORK
