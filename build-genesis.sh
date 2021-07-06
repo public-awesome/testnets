8e98a33e92d3d31d0f70a318f8087b859020d13d
Shane Vitarana <shanev@gmail.com>
2021-07-05 22:30:13 -0400

    add vesting accounts

--- build-genesis.sh
+++ build-genesis.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 DENOM=ustarx
-NETWORK=cygnusx-1
-
-GENESIS_TIME=2021-05-25T00:44:44.536813Z
-START_TIME=$(TZ=UTC date -j -f "%Y-%m-%dT%H:%M:%S" ${GENESIS_TIME:0:19} +%s)
+CHAIN_ID=cygnusx-1
 ONE_HOUR=3600
 ONE_DAY=$(($ONE_HOUR * 24))
 ONE_YEAR=$(($ONE_DAY * 365))
-vesting_start_time=$(($START_TIME + $ONE_DAY))
-vesting_end_time=$(($vesting_start_time + $ONE_DAY))
+
+if [ "$1" == "mainnet" ]
+then
+    LOCKUP=ONE_YEAR
+else
+    LOCKUP=ONE_DAY
+fi
+echo "Lockup period is $LOCKUP"
 
 rm -rf ~/.starsd
 
@@ -18,20 +21,28 @@ if ! [ -f genesis.json ]; then
 fi
 starsd export-airdrop-snapshot uatom genesis.json snapshot.json
 
-starsd init testmoniker --stake-denom $DENOM --chain-id $NETWORK
+starsd init testmoniker --stake-denom $DENOM --chain-id $CHAIN_ID
 
 starsd import-genesis-accounts-from-snapshot snapshot.json
 
-starsd prepare-genesis testnet $NETWORK
+# FIXME: THIS REMOVES ALL BALANCES
+starsd prepare-genesis testnet $CHAIN_ID
 
-starsd add-genesis-account stars1s4ckh9405q0a3jhkwx9wkf9hsjh66nmuu53dwe 333333333333333ustarx \
-    --vesting-amount 333333332333333ustarx \
-    --vesting-start-time $vesting_start_time \
-    --vesting-end-time $vesting_end_time
+# # add vesting accounts
+# GENESIS_TIME=$(jq '.genesis_time' ~/.starsd/config/genesis.json | tr -d '"')
+# echo "Genesis time is $GENESIS_TIME"
+# GENESIS_UNIX_TIME=$(TZ=UTC date -j -f "%Y-%m-%dT%H:%M:%S" ${GENESIS_TIME:0:19} +%s)
+# vesting_start_time=$(($GENESIS_UNIX_TIME + $LOCKUP))
+# vesting_end_time=$(($vesting_start_time + $LOCKUP))
 
-starsd validate-genesis
+# starsd add-genesis-account stars1s4ckh9405q0a3jhkwx9wkf9hsjh66nmuu53dwe 333333333333333ustarx \
+#     --vesting-amount 333333332333333ustarx \
+#     --vesting-start-time $vesting_start_time \
+#     --vesting-end-time $vesting_end_time
+
+# starsd validate-genesis
 
 # echo "Writing validator transactions..."
 # sh ./validators.sh
 
-# cp ./starsd/config/genesis.json $NETWORK
+# cp ./starsd/config/genesis.json $CHAIN_ID
