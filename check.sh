d56e13539fdfe92c69a612546d6a3f06a4fb6a86
jhernandezb <contact@jhernandez.me>
2021-07-06 23:13:48 -0600

    check script

--- check.sh
+++ check.sh
@@ -2,6 +2,10 @@
 FILE=./check-gen-tx
 if [ -f "$FILE" ]; then
     echo "checking gentx collect"
+    export FILE_LOCATION=$(cat check-gen-tx-file)
+    starsd init validator
+    mkdir -p ~/.starsd/config/gentx
+    cp "./$FILE_LOCATION" ~/.starsd/config/gentx/
     export ADDRESS=$(cat check-gen-tx)
     starsd add-genesis-account $ADDRESS 1000000000000000ustarx
     starsd collect-gentxs
