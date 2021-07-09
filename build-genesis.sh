5379f612ec4c1fe15c2171ed90edda6a70231fc3
Shane Vitarana <shanev@gmail.com>
2021-07-09 17:09:44 -0400

    Add vesting accounts to genesis (#564)
    * download hub-4 archive
    
    * updating build genesis script
    
    * add gitignore
    
    * add vesting accounts
    
    * process validator accounts
    
    * Fixed typo
    
    * Use GNU date in script (#506)
    
    * Use GNU date in script
    
    Script wasn't working on Linux. Updated to use GNU date rather than
    MacOS version.
    
    * Fix typo
    
    * Updated vesting accounts
    
    * Update accounts
    
    * Updated chain init command
    
    * Add community pool funder
    
    * Remove community pool funder
    
    * Add back community pool funder
    
    Co-authored-by: Jake Hartnell <Jake.Hartnell@gmail.com>

--- build-genesis.sh
+++ build-genesis.sh
@@ -1,12 +1,72 @@
 #!/bin/bash
 
-CONFIG=~/.starsd/config
-NETWORK=cygnusx-1
+DENOM=ustarx
+CHAIN_ID=cygnusx-1
+ONE_HOUR=3600
+ONE_DAY=$(($ONE_HOUR * 24))
+ONE_YEAR=$(($ONE_DAY * 365))
+VALIDATOR_COINS=1000000000000$DENOM
 
-# echo "Writing genesis accounts..."
-# sh ./accounts.sh
+if [ "$1" == "mainnet" ]
+then
+    LOCKUP=ONE_YEAR
+else
+    LOCKUP=ONE_DAY
+fi
+echo "Lockup period is $LOCKUP"
 
-echo "Writing validator transactions..."
-sh ./validators.sh
+rm -rf ~/.starsd
 
-cp $CONFIG/genesis.json $NETWORK
+echo "Processing airdrop snapshot..."
+if ! [ -f genesis.json ]; then
+    curl -O https://archive.interchain.io/4.0.2/genesis.json
+fi
+starsd export-airdrop-snapshot uatom genesis.json snapshot.json
+starsd init testmoniker --chain-id $CHAIN_ID
+starsd prepare-genesis testnet $CHAIN_ID
+starsd import-genesis-accounts-from-snapshot snapshot.json
+
+echo "Adding vesting accounts..."
+GENESIS_TIME=$(jq '.genesis_time' ~/.starsd/config/genesis.json | tr -d '"')
+echo "Genesis time is $GENESIS_TIME"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    GENESIS_UNIX_TIME=$(TZ=UTC gdate "+%s" -d $GENESIS_TIME)
+else
+    GENESIS_UNIX_TIME=$(TZ=UTC date "+%s" -d $GENESIS_TIME)
+fi
+vesting_start_time=$(($GENESIS_UNIX_TIME + $LOCKUP))
+vesting_end_time=$(($vesting_start_time + $LOCKUP))
+
+starsd add-genesis-account stars1s4ckh9405q0a3jhkwx9wkf9hsjh66nmuu53dwe 350000000000000$DENOM
+starsd add-genesis-account stars13nh557xzyfdm6csyp0xslu939l753sdlgdc2q0 250000000000000$DENOM
+starsd add-genesis-account stars12yxedm78tpptyhhasxrytyfyj7rg7dcqfgrdk4 16666666666667$DENOM \
+    --vesting-amount 16666666666667$DENOM \
+    --vesting-start-time $vesting_start_time \
+    --vesting-end-time $vesting_end_time
+starsd add-genesis-account stars1nek5njjd7uqn5zwf5zyl3xhejvd36er3qzp6x3 16666666666667$DENOM \
+    --vesting-amount 16666666666667$DENOM \
+    --vesting-start-time $vesting_start_time \
+    --vesting-end-time $vesting_end_time
+starsd add-genesis-account stars1avlcqcn4hsxrds2dgxmgrj244hu630kfl89vrt 16666666666667$DENOM \
+    --vesting-amount 16666666666667$DENOM \
+    --vesting-start-time $vesting_start_time \
+    --vesting-end-time $vesting_end_time
+starsd add-genesis-account stars1wppujuuqrv52atyg8uw3x779r8w72ehrr5a4yx 50000000000000$DENOM \
+    --vesting-amount 50000000000000$DENOM \
+    --vesting-start-time $vesting_start_time \
+    --vesting-end-time $vesting_end_time
+
+echo "Processing validators..."
+mkdir -p ~/.starsd/config/gentx
+for i in $NETWORK/gentx/*.json; do
+    echo $i
+    starsd add-genesis-account $(jq -r '.body.messages[0].delegator_address' $i) $VALIDATOR_COINS \
+        --vesting-amount $VALIDATOR_COINS \
+        --vesting-start-time $vesting_start_time \
+        --vesting-end-time $vesting_end_time
+    cp $i ~/.starsd/config/gentx/
+done
+starsd collect-gentxs
+starsd validate-genesis
+
+cp ~/.starsd/config/genesis.json $CHAIN_ID
