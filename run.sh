b0b9676ee3ddd417ff9b3da637a45af37b8fe43d
jhernandezb <contact@jhernandez.me>
2021-07-13 10:45:51 -0600

    fix start time

--- run.sh
+++ run.sh
@@ -7,31 +7,28 @@ ONE_DAY=$(($ONE_HOUR * 24))
 ONE_YEAR=$(($ONE_DAY * 365))
 VALIDATOR_COINS=1000000000000$DENOM
 
-if [ "$1" == "mainnet" ]
-then
-    LOCKUP=ONE_YEAR
-else
-    LOCKUP=ONE_DAY
-fi
-echo "Lockup period is $LOCKUP"
-
-rm -f ~/.starsd/config/genesis.json
-rm -f ~/.starsd/config/gentx/*
-
-echo "Adding vesting accounts..."
-GENESIS_TIME=$(jq '.genesis_time' ~/.starsd/config/genesis.json | tr -d '"')
-echo "Genesis time is $GENESIS_TIME"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    GENESIS_UNIX_TIME=$(TZ=UTC gdate "+%s" -d $GENESIS_TIME)
-else
-    GENESIS_UNIX_TIME=$(TZ=UTC date "+%s" -d $GENESIS_TIME)
-fi
-vesting_start_time=$(($GENESIS_UNIX_TIME + $LOCKUP))
-vesting_end_time=$(($vesting_start_time + $LOCKUP))
-
 
 if [ -f "$FILE" ]; then
     starsd init validator --chain-id cygnusx-1
+    if [ "$1" == "mainnet" ]
+    then
+        LOCKUP=ONE_YEAR
+    else
+        LOCKUP=ONE_DAY
+    fi
+    echo "Lockup period is $LOCKUP"
+
+    echo "Adding vesting accounts..."
+    GENESIS_TIME=$(jq '.genesis_time' ~/.starsd/config/genesis.json | tr -d '"')
+    echo "Genesis time is $GENESIS_TIME"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        GENESIS_UNIX_TIME=$(TZ=UTC gdate "+%s" -d $GENESIS_TIME)
+    else
+        GENESIS_UNIX_TIME=$(TZ=UTC date "+%s" -d $GENESIS_TIME)
+    fi
+    vesting_start_time=$(($GENESIS_UNIX_TIME + $LOCKUP))
+    vesting_end_time=$(($vesting_start_time + $LOCKUP))
+
     sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' ~/.starsd/config/config.toml
     mkdir -p ~/.starsd/config/gentx
     echo "Processing validators..."
