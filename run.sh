4999738e833f47f5c06483d223956ca41a8a0580
jhernandezb <contact@jhernandez.me>
2021-07-13 09:30:58 -0600

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #/bin/sh
 FILE=./check-gen-tx
 if [ -f "$FILE" ]; then
+    echo "checking gentx collect"
+    export FILE_LOCATION=$(cat check-gen-tx-file)
+    starsd init validator --chain-id cygnusx-1
+    mkdir -p ~/.starsd/config/gentx
+    cp "./$FILE_LOCATION" ~/.starsd/config/gentx/
+    export ADDRESS=$(cat check-gen-tx)
+    starsd add-genesis-account $ADDRESS 1000000000000000ustarx
+    starsd collect-gentxs
     starsd start &
     sleep 30
 fi
