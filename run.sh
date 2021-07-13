09d9ce8a21cd3d0db5990bb4681352756a59abb5
jhernandezb <contact@jhernandez.me>
2021-07-13 09:56:20 -0600

    check all

--- run.sh
+++ run.sh
@@ -1,14 +1,23 @@
 #/bin/sh
 FILE=./check-gen-tx
+DENOM=ustarx
+CHAIN_ID=cygnusx-1
 if [ -f "$FILE" ]; then
     echo "checking gentx collect"
     export FILE_LOCATION=$(cat check-gen-tx-file)
     starsd init validator --chain-id cygnusx-1
     mkdir -p ~/.starsd/config/gentx
-    cp "./$FILE_LOCATION" ~/.starsd/config/gentx/
-    export ADDRESS=$(cat check-gen-tx)
-    starsd add-genesis-account $ADDRESS 1000000000000000ustarx
+    echo "Processing validators..."
+    for i in $CHAIN_ID/gentx/*.json; do
+        echo $i
+        starsd add-genesis-account $(jq -r '.body.messages[0].delegator_address' $i) $VALIDATOR_COINS \
+            --vesting-amount $VALIDATOR_COINS \
+            --vesting-start-time $vesting_start_time \
+            --vesting-end-time $vesting_end_time
+        cp $i ~/.starsd/config/gentx/
+    done
     starsd collect-gentxs
+    starsd validate-genesis
     starsd start &
-    sleep 30
+    sleep 60
 fi
